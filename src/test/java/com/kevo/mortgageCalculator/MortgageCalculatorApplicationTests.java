e17f03486601f44c3f4d6f6779b646b3f05ffdf2
Kevin Koomson <andrew.koomson@amalitech.com>
2024-05-21 10:52:47 +0000

    init

---
+++ src/test/java/com/kevo/mortgageCalculator/MortgageCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevo.mortgageCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
